51297f2dbf3d5ea4ca20b168d77a90722f86a5eb
ligiacr <ligia_c_rodrigues@hotmail.com>
2022-01-25 13:33:30 -0300

    Adiciona Set1

---
+++ src/me/dio/collections/set/Set1.java
@@ -0,0 +1,71 @@
+package me.dio.collections.set;
+
+import java.util.*;
+
+public class Set1 {
+    public static void main(String[] args) {
+
+        Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        System.out.println(notas);
+
+        /*System.out.println("Exiba a posição da nota 5.0:");
+        System.out.println("Adicione na lista a nota 8.0 na posição 4:");
+        System.out.println("Substitua a nota 5.0 pela nota 6.0:");
+        System.out.println("Exiba a terceira nota adicionada:");
+        System.out.println("Remova a nota na posição 0: ");
+
+        Não há como fazer isso usando o HashSet*/
+
+        System.out.println("Confira se a nota 5.0 está no conjunto: " + notas.contains(5d));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        System.out.println("Exiba a soma dos valores:");
+        double soma = 0.0;
+        Iterator<Double> iterator = notas.iterator();
+        while (iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println(soma);
+
+        System.out.println("Exiba a média das notas: " + (soma / notas.size()));
+
+        System.out.println("Remova a nota 0:");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba na lista:");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if (next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Exiba todas as notas na ordem em que foram informadas:");
+        Set<Double> notas2 = new LinkedHashSet<>();
+        notas2.add(7d);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5d);
+        notas2.add(7d);
+        notas2.add(0d);
+        notas2.add(3.6);
+        System.out.println(notas2); //não exibirá números repetidos
+
+        System.out.println("Exiba todas as notas na ordem crescente:");
+        Set<Double> notas3 = new TreeSet<>(notas2);
+        System.out.println(notas3);
+
+        System.out.println("Apague todo o conjunto:");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("Confira se o conjunto está vazio: " + notas.isEmpty());
+        System.out.println("Confira se o conjunto 2 está vazio: " + notas2.isEmpty());
+        System.out.println("Confira se o conjunto 3 está vazio: " + notas3.isEmpty());
+    }
+}
